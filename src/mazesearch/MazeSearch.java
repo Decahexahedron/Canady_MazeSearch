730a2304e765dfb95fd64003aae735fcb11fc57c
Rhys Canady <rhysdaniel064@gmail.com>
2016-11-28 09:02:48 -0600

    002

--- src/mazesearch/MazeSearch.java
+++ src/mazesearch/MazeSearch.java
@@ -5,7 +5,7 @@ public class MazeSearch {
     static Maze labyrinth = new Maze();
 
     public static void main(String[] args) {
-        labyrinth.traverse(0, 0);
+//        labyrinth.traverse(0, 0);
         labyrinth.print(labyrinth.grid);
     }
 
