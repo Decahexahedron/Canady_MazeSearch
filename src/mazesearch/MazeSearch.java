590f6dc01818bcf82a6fec8a1c20e303b8299089
Rhys Canady <rhysdaniel064@gmail.com>
2016-11-28 09:18:04 -0600

    003

--- src/mazesearch/MazeSearch.java
+++ src/mazesearch/MazeSearch.java
@@ -4,9 +4,9 @@ public class MazeSearch {
 
     static Maze labyrinth = new Maze();
 
-    public static void main(String[] args) {
-//        labyrinth.traverse(0, 0);
-        labyrinth.print(labyrinth.grid);
+    public static void main(String[] args) throws InterruptedException {
+        labyrinth.traverse(0, 0);
+        labyrinth.print();
     }
 
 }
