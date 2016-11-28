d3a94b14e330b9b89ee20ebc04734d75a870e698
Rhys Canady <rhysdaniel064@gmail.com>
2016-11-28 09:01:33 -0600

    new better maze

--- src/mazesearch/Maze.java
+++ src/mazesearch/Maze.java
@@ -4,7 +4,7 @@ public class Maze {
 
     private final int tried = 2;
     private final int path = 3;
-    static int[][] grid
+    public static int[][] grid
             = {{1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1},
             {1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1},
             {0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0},
@@ -14,19 +14,36 @@ public class Maze {
             {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
             {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
 
-    boolean traverse() throws InterruptedException {
-        if (0 == 1) {
-            return true;
+    public boolean traverse(int row, int column) throws InterruptedException {
+
+        if (valid(row, column)) {
+            grid[row][column] = tried;
+        } else {
+            return false;
         }
-        return false;
+
+        if (true) {
+            grid[row][column] = path;
+        }
+        
+        for (int i = 0; i < 100; i++) {
+            System.out.println("");
+        }
+        print(grid);
+        Thread.sleep(500);
+        return true;
     }
 
-    void print() {
-        for (int i = 0; i < grid[0].length; i++) {
+    private boolean valid(int row, int column) throws InterruptedException {
+        return row < grid.length && row >= 0 && column < grid[0].length && column >= 0;
+    }
+
+    void print(int[][] grid) {
+        for (int i = 0; i < grid[0].length - 5; i++) {
             for (int j = 0; j < grid[1].length; j++) {
                 System.out.print(grid[i][j]);
             }
-            System.out.println("");
+            System.out.print("\n");
         }
     }
 }
