e6c80f20833b1d1175bbf00ec42498cd55b8bbee
Rhys Canady <rhysdaniel064@gmail.com>
2016-11-29 09:18:59 -0600

    004

--- src/mazesearch/Maze.java
+++ src/mazesearch/Maze.java
@@ -3,7 +3,7 @@ package mazesearch;
 public class Maze {
 
     private final int tried = 2;
-    private final int path = 3;
+    private final int path = 4;
     public static int[][] grid
             = {{1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1},
             {1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1},
@@ -22,26 +22,35 @@ public class Maze {
             return false;
         }
 
-        if (true) {
+        if (traverse(row - 1, column) || traverse(row + 1, column) || traverse(row, column - 1)
+                || traverse(row, column + 1) || row == grid[0].length && column == grid[1].length) { //fix recursion
             grid[row][column] = path;
+            print();
+            return true;
         }
-        
-        for (int i = 0; i < 100; i++) {
+
+        for (int i = 0; i < 10; i++) {
             System.out.println("");
         }
-        print(grid);
-        Thread.sleep(500);
-        return true;
+
+        print();
+        Thread.sleep(200);
+
+        if (grid[7][12] == tried || grid[7][12] == path) {
+            return true;
+        }
+
+        return false;
     }
 
     private boolean valid(int row, int column) throws InterruptedException {
-        return row < grid.length && row >= 0 && column < grid[0].length && column >= 0;
+        return row < grid.length && row >= 0 && column < grid[0].length && column >= 0 && grid[row][column] == 1;
     }
 
-    void print(int[][] grid) {
+    void print() {
         for (int i = 0; i < grid[0].length - 5; i++) {
             for (int j = 0; j < grid[1].length; j++) {
-                System.out.print(grid[i][j]);
+                System.out.print(grid[i][j] + " ");
             }
             System.out.print("\n");
         }
--- src/mazesearch/MazeSearch.java
+++ src/mazesearch/MazeSearch.java
@@ -5,9 +5,9 @@ public class MazeSearch {
     static Maze labyrinth = new Maze();
 
     public static void main(String[] args) throws InterruptedException {
-        if(labyrinth.traverse(0, 0)){
+        if (labyrinth.traverse(0, 0)) {
             System.out.println("Maze solved!");
-        } else{
+        } else {
             System.out.println("Maze not possible!");
         }
         labyrinth.print();
